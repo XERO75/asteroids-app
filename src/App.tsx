0d89b08c6e7b6c338ceb178768d39ee0f86141f5
xavi <xerolry@gmail.com>
2024-04-26 00:15:56 +0800

    style: add iconfont and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './App.css';
-
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="flex items-center justify-center bg-dark min-h-screen">
+      <h1 className="text-3xl font-bold underline text-green">hello wolrd</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      white: '#fff',
+      gray: '#9499C3',
+      red: '#EB5757',
+      green: '#00CF67',
+      dark: '#1A1B2F',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
